af5183c4266e2b2d88eb692177d297ff779e719d
ShiSFC <ssfci@qq.com>
2022-04-03 14:00:10 +0800

    master second

--- TuShuGuan/app01/urls.py
+++ TuShuGuan/app01/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from app01.views import *
 
 urlpatterns = [
-    path('menu/book/', book, name='book_show'),
+    path('menu/book/', book, name='book_show'),    # 书籍
     path('menu/book/add/', book_add, name='book_add'),
     path('menu/book/edit/', book_edit, name='book_edit'),
     path('menu/book/del/', book_del, name='book_del'),
@@ -17,6 +17,11 @@ urlpatterns = [
     path('menu/borrowingLog/del/', borrowingLog_del, name='borrowingLog_del'),
     path('menu/borrowingLog/return/', borrowingLog_return, name='borrowingLog_return'),
 
+    path('menu/reader/', reader, name='reader_show'),   # 读者
+    path('menu/reader/add/', reader_add, name='reader_add'),
+    path('menu/reader/edit/', reader_edit, name='reader_edit'),
+    path('menu/reader/del/', reader_del, name='reader_del'),
+
     path('menu/author/', author, name='author_show'),  # 作者
     path('menu/author/add/', author_add, name='author_add'),
     path('menu/author/edit/', author_edit, name='author_edit'),
@@ -26,11 +31,5 @@ urlpatterns = [
     path('menu/publish/add/', publish_add, name='publish_add'),
     path('menu/publish/edit/', publish_edit, name='publish_edit'),
     path('menu/publish/del/', publish_del, name='publish_del'),
-
-
-    path('menu/reader/', reader, name='reader_show'),
-    path('menu/reader/add/', reader_add, name='reader_add'),
-    path('menu/reader/edit/', reader_edit, name='reader_edit'),
-    path('menu/reader/del/', reader_del, name='reader_del'),
 
 ]
